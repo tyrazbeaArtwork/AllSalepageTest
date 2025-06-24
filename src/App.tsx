6d4aacdb4655d985ca4c700319393980fe6ae52b
tyrazbea <ainazaini25@gmail.com>
2025-06-24 20:51:11 +0800

    Updattteee

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainNavigation from "./pages/MainNavigation";
 import OfficialLanding from "./pages/OfficialLanding";
 import CustomLanding from "./pages/CustomLanding";
@@ -26,7 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       {/* Add the basename prop here, matching your repository name */}
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<MainNavigation />} />
           <Route path="/official" element={<OfficialLanding />} />
@@ -43,7 +43,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
